6fb8ab3f51d2469d4f8af45f44c3d3445caa70e4
Aplusplus <njuaplusplus@gmail.com>
2015-02-18 22:49:25 +0800

    Add reply for wechat  event

--- wechat/views.py
+++ wechat/views.py
@@ -1,24 +1,42 @@
 #!/usr/local/bin/python
 # coding=utf-8
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from django.utils.encoding import smart_str
 
 import hashlib, urllib2, json
 
 def index(request):
     if request.method == 'GET':
         return HttpResponse(checkSignature(request))
-def checkSignature(request):
-    if request.method == 'GET':
-        signature=request.GET.get('signature',None)
-        timestamp=request.GET.get('timestamp',None)
-        nonce=request.GET.get('nonce',None)
-        echostr=request.GET.get('echostr',None)
     else:
-        signature=request.POST.get('signature',None)
-        timestamp=request.POST.get('timestamp',None)
-        nonce=request.POST.get('nonce',None)
-        echostr=request.POST.get('echostr',None)
+        if checkSignature(request):
+            import xml.etree.ElementTree as ET
+            import time
+            rawStr = smart_str(request.body)
+            msg = paraseMsgXml(ET.fromstring(rawStr))
+            toUser = msg.get('FromUserName','')
+            fromUser = msg.get('ToUserName','')
+            createTime = str(int(time.time()))
+            #msgType = ''.join(msg.get('MsgType', '').split()) # QQ 居然返回的 text 前后有空白符..
+            msgType = msg.get('MsgType', '')
+
+            if msgType == 'event':
+                event = msg['Event']
+                if event == 'subscribe':
+                    content = u'就接收小视频而已..'
+                    context = { 'toUser': toUser, 'fromUser': fromUser, 'createTime': createTime, 'content': content}
+                    return render_to_response('wechat/reply_text.xml', context, content_type="application/xml")
+                elif event == 'unsubscribe':
+                    content = u'......'
+                    context = { 'toUser': toUser, 'fromUser': fromUser, 'createTime': createTime, 'content': content}
+                    return render_to_response('wechat/reply_text.xml', context, content_type="application/xml")
+
+def checkSignature(request):
+    signature=request.GET.get('signature',None)
+    timestamp=request.GET.get('timestamp',None)
+    nonce=request.GET.get('nonce',None)
+    echostr=request.GET.get('echostr',None)
 
     #这里的token我放在setting，可以根据自己需求修改
     token='njuaplusplus'
@@ -31,3 +49,10 @@ def checkSignature(request):
         return echostr
     else:
         return None
+
+def paraseMsgXml(rootElem):
+    msg = {}
+    if rootElem.tag == 'xml':
+        for child in rootElem:
+            msg[child.tag] = smart_str(child.text).strip()
+    return msg
