b5f088af098ef4874368cb3ae6bb4d9183c9bb13
Aplusplus <njuaplusplus@gmail.com>
2015-02-24 14:36:27 +0800

    random image size

--- photos/views.py
+++ photos/views.py
@@ -10,7 +10,8 @@ def index(request):
 
 def index_page(request, page_num):
     holderjs_theme = random.choice(['sky', 'lava', 'social', 'vine', 'gray', 'industrial'])
-    num_of_img = random.randrange(1,9)
+    num_of_img = xrange(random.randrange(1,9))
+    num_of_img = [ "%dx%d" % (random.randrange(200, 600), random.randrange(200, 600)) for i in num_of_img ]
     template = 'photos/index.html',
     if request.user_agent.is_mobile:
         template = 'photos/mobile/index.html'
@@ -19,7 +20,7 @@ def index_page(request, page_num):
         template,
         {
             'holderjs_theme' : holderjs_theme,
-            'num_of_img' : xrange(num_of_img),
+            'num_of_img' : num_of_img,
             'next_page_num' : str(int(page_num) + 1),
         }
     )
