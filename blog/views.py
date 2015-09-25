7fb8b8782d45376896f94049c61b497fd1613d33
Aplusplus <njuaplusplus@gmail.com>
2015-09-25 21:28:21 +0800

    Add date archive page

--- blog/views.py
+++ blog/views.py
@@ -45,7 +45,7 @@ def evenly_divide_list(l, n=2):
 
 def index_page(request, page_num):
     """The news index"""
-    # archive_dates = Article.objects.datetimes('date_publish','month', order='DESC')
+    archive_dates = evenly_divide_list(Article.objects.datetimes('date_publish','month', order='DESC'))
     categories = evenly_divide_list(get_top_categories(8))
 
     article_queryset = Article.objects.all()
@@ -64,9 +64,9 @@ def index_page(request, page_num):
         request,
         "blog/default/index.html",
         {
-            "articles" : articles,
-            # "archive_dates" : archive_dates,
-            "categories" : categories
+            "articles"      : articles,
+            "archive_dates" : archive_dates,
+            "categories"    : categories,
         }
     )
 
@@ -85,7 +85,10 @@ def single(request, slug) :
         }
     )
 
-def date_archive(request, year, month) :
+def date_archive(request, year, month):
+    return date_archive_page(request, year, month, 1)
+
+def date_archive_page(request, year, month, page_num):
     """The blog date archive"""
     # this archive pages dates
     year = int(year)
@@ -93,16 +96,13 @@ def date_archive(request, year, month) :
     month_range = calendar.monthrange(year, month)
     start = datetime.datetime(year=year, month=month, day=1)#.replace(tzinfo=utc)
     end = datetime.datetime(year=year, month=month, day=month_range[1])#.replace(tzinfo=utc)
-    archive_dates = Article.objects.datetimes('date_publish','month', order='DESC')
-    categories = Category.objects.all()
 
     # Pagination
-    page = request.GET.get('page')
     article_queryset = Article.objects.filter(date_publish__range=(start.date(), end.date()))
     paginator = Paginator(article_queryset, 5)
 
     try:
-        articles = paginator.page(page)
+        articles = paginator.page(page_num)
     except PageNotAnInteger:
         # If page is not an integer, deliver first page.
         articles = paginator.page(1)
@@ -112,13 +112,13 @@ def date_archive(request, year, month) :
 
     return render(
         request,
-        "blog/article/date_archive.html",
+        "blog/default/date_archive.html",
         {
-            "start" : start,
-            "end" : end,
-            "articles" : articles,
-            "archive_dates" : archive_dates,
-            "categories" : categories
+            "start"         : start,
+            "end"           : end,
+            "articles"      : articles,
+            "archive_dates" : evenly_divide_list(Article.objects.datetimes('date_publish','month', order='DESC')),
+            "categories"    : evenly_divide_list(get_top_categories(8)),
         }
     )
 
@@ -145,10 +145,10 @@ def category_archive_page(request, slug, page_num):
         request,
         "blog/default/category_archive.html",
         {
-            "articles"   : articles,
-            # "archive_dates" : archive_dates,
-            'categories' : evenly_divide_list(get_top_categories(8)),
-            "category"   : category,
+            "articles"      : articles,
+            "archive_dates" : evenly_divide_list(Article.objects.datetimes('date_publish','month', order='DESC')),
+            'categories'    : evenly_divide_list(get_top_categories(8)),
+            "category"      : category,
         }
     )
 
